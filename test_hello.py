bfb9230ab286ba18751be9a80f9846d940ed200e
soumyaxm <131882450+soumyaxm@users.noreply.github.com>
2024-06-20 13:10:25 +0530

    added script and test

---
+++ test_hello.py
@@ -0,0 +1,9 @@
+import unittest
+from hello import hello_world
+
+class TestHelloWorld(unittest.TestCase):
+    def test_hello_world(self):
+        self.assertEqual(hello_world(), "Hello, world!")
+
+if __name__ == '__main__':
+    unittest.main()
